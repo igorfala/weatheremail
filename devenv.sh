21fa6359b5a50b9cf7c74e2856479fb1bac70f53
Igor Fala <ifala88@gmail.com>
2017-11-09 00:58:52 +0000

    Initial commit
    - create gitignore
    - create devenv.sh to enter a new shell
    - install Django==1.1.17
    - create Makefile
    - create django project weatheremail
    - create django app subscriptions
    - pip freeze >> requirements.txt
    - install flake8

---
+++ devenv.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+cleanup() {
+	[ -n "${bashrc}" ] && rm -f "${bashrc}"
+}
+
+trap cleanup HUP TERM EXIT
+
+topdir=$(readlink -f $(dirname $0))
+PYTHON_VERSION=${PYTHON_VERSION:-3.5}
+bashrc=$(TMPDIR=${topdir} mktemp .devrc-XXXXXX)
+make=$(which gmake 2>/dev/null || which make)
+
+if [ -n "${VIRTUAL_ENV}" ]; then
+	virtualenv=$(readlink -f ${VIRTUAL_ENV})
+else
+	virtualenv=${topdir}/virtualenv${PYTHON_VERSION}
+fi
+venv_devdir=${virtualenv}/weather-dev
+
+if [ ! -d ${virtualenv} ]; then
+	virtualenv --python=python${PYTHON_VERSION} ${virtualenv} || exit 1
+fi
+
+# Install requirements
+[ ! -d ${venv_devdir} ] && mkdir -p ${venv_devdir}
+for req_path in ${topdir}/requirements*.txt; do
+	req_basename=$(basename ${req_path})
+	req_status=${venv_devdir}/${req_basename}
+	if [ ${req_status} -ot ${req_path} ]; then
+		(source ${virtualenv}/bin/activate && ${make} -C ${topdir} ${req_basename})
+		touch -m ${req_status}
+	fi
+done
+
+# Instructions to run when entering new shell
+cat > ${bashrc} <<-EOF
+	[ -f ~/.bash_profile ] && source ~/.bash_profile
+	source ${virtualenv}/bin/activate
+	${make} -C ${topdir}/weatheremail
+EOF
+
+# Enter developmet shell
+if [ -z "$*" ]; then
+	/usr/bin/env bash --rcfile ${bashrc} -i
+else
+	source ${bashrc}
+	$*
+fi
+
+# vim: noet
